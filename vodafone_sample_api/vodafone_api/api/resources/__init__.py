6290006b271afdee968cb3ef95b43b38b947f368
Grigorios Markopoulos <grigorios.markopoulos@underwriteme.co.uk>
2020-02-02 20:09:59 +0000

    Add API scaffold generated with cookiecutter

---
+++ vodafone_sample_api/vodafone_api/api/resources/__init__.py
@@ -0,0 +1,4 @@
+from vodafone_api.api.resources.user import UserResource, UserList
+
+
+__all__ = ["UserResource", "UserList"]
---
+++ vodafone_sample_api/vodafone_api/models/__init__.py
@@ -0,0 +1,5 @@
+from vodafone_api.models.user import User
+from vodafone_api.models.blacklist import TokenBlacklist
+
+
+__all__ = ["User", "TokenBlacklist"]
