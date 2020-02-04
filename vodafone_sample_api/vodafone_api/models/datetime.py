a4143f0bb9cefd0a5cc2b0906dc527e9a895dab3
Grigorios Markopoulos <grigorios.markopoulos@underwriteme.co.uk>
2020-02-04 22:42:05 +0000

    Fix db model for datetime

--- vodafone_sample_api/vodafone_api/models/datetime.py
+++ vodafone_sample_api/vodafone_api/models/datetime.py
@@ -3,8 +3,8 @@ from vodafone_api.extensions import db
 
 class Datetime(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    datetime = db.Column(db.Datetime, nullable=False)
-    comment = db.Column(db.string())
+    datetime = db.Column(db.DATETIME, nullable=False)
+    comment = db.Column(db.VARCHAR(1000))
 
     def __init__(self, **kwargs):
         super(USer, self).__init__(**kwargs)
