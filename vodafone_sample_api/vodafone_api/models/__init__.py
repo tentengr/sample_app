9534a1ee90acd810ae6b5dfba36958c74b13ab29
Grigorios Markopoulos <grigorios.markopoulos@underwriteme.co.uk>
2020-02-04 22:27:46 +0000

    Expose datetime db model

--- vodafone_sample_api/vodafone_api/models/__init__.py
+++ vodafone_sample_api/vodafone_api/models/__init__.py
@@ -1,5 +1,6 @@
 from vodafone_api.models.user import User
 from vodafone_api.models.blacklist import TokenBlacklist
+from vodafone_api.models.datetime import Datetime
 
 
-__all__ = ["User", "TokenBlacklist"]
+__all__ = ["User", "TokenBlacklist", "Datetime"]
